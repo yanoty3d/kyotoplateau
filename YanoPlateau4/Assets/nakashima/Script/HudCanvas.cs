aa03f1ee908bc2f04ecca7c66681a98b7851bdeb
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2025-01-19 00:32:06 +0900

    制限時間終了時にシーン遷移する処理を追加

--- YanoPlateau4/Assets/nakashima/Script/HudCanvas.cs
+++ YanoPlateau4/Assets/nakashima/Script/HudCanvas.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 public class HudCanvas : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI remaning_time_text;
-    public float max_remaning_time;
-    float remaning_time;
-    void Start()
-    {
-        remaning_time = max_remaning_time;
-    }
-
     void Update()
     {
-        remaning_time -= Time.deltaTime;
-        remaning_time = Mathf.Max(remaning_time, 0.0f);
-        remaning_time_text.text = remaning_time.ToString("0.0");
+        if (MainGameManager.Instance)
+        {
+            remaning_time_text.text = MainGameManager.Instance.RemaningTime.ToString("0.0");
+        }
     }
 }
---
+++ YanoPlateau4/Assets/nakashima/Script/MainGameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainGameManager : MonoSingleton<MainGameManager>
+{
+    public string result_scene_name;
+    public float max_remaning_time;
+    public float RemaningTime { get; private set; }
+    bool game_playing;
+    void Start()
+    {
+        RemaningTime = max_remaning_time;
+        StartGameTimer();
+    }
+
+    public void StartGameTimer()
+    {
+        game_playing = true;
+    }
+
+    void Update()
+    {
+        if (!game_playing) { return; }
+
+        RemaningTime -= Time.deltaTime;
+        RemaningTime = Mathf.Max(RemaningTime, 0.0f);
+        if(RemaningTime <= 0.0f)
+        {
+            print("game end");
+            //SceneManager.LoadScene(result_scene_name);
+            game_playing = false;
+        }
+    }
+}
